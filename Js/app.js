200bb7b96e9e3635a5bf622d32d3e25a16e1f1c4
CavadovT <turalkhj@code.edu.az>
2022-05-23 17:31:44 +0400

    first commit

---
+++ Js/app.js
@@ -0,0 +1,62 @@
+/*
+bir string veeilir ve o stringde hansi simvoldan nece dene varsa onun sayinini
+teyin eden bir method yazilir....
+*/
+const str = "hhhhkjghjklgffk.jh;ljigyfyokiytdtrxrkhjhkjkjlklklljjhh";
+let arr = [];
+for (let i = 0; i < str.length; i++) {
+  arr.push(str[i]);
+}
+
+let CountOfChars = arr.reduce((allChars, item) => {
+  if (item in allChars) {
+    allChars[item]++;
+  } else {
+    allChars[item] = 1;
+  }
+  return allChars;
+}, {});
+console.log(CountOfChars);
+
+/*
+ 1) Bir class yaradirsiz ve o classin icerisinde plus, minus, multiply, divide functionlari olur.
+         var result = new CustomMatch(50).plus(6).minus(30).multiply(3).divide(2)
+         resultin neticesi bu yazilisa gore 50+6-30*3/2 mentiqi ile 
+         (riyazi prinsibi unudun,vurma bolme onceliyine ehtiyac yoxdu) 39 olmalidi
+*/
+class CustomMatchx {
+  constructor(num) {
+    this.num = num;
+  }
+  plus(num) {
+    this.num =this.num+ num;
+    return this;
+  }
+  minus(num) {
+    this.num = this.num - num;
+    return this;
+  }
+  multiple(num) {
+    this.num =this.num * num;
+    return this;
+  }
+  divide(num){
+      this.num=this.num/num;
+      return this;
+  }
+  toString(){
+      return this.num;
+  }
+  
+}
+let result = new CustomMatchx(50).plus(6).minus(30).multiple(3).divide(2);
+
+console.log(result.toString());
+
+
+/*
+ Custom bir array classi yaradin.
+ Hemin arrayin find ve push metodu olsun.
+*/
+
+
